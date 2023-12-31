d1756b430202b7a542ba1cc36ce7f878177a4835
SaadAlsayed <smqaa.github@gmail.com>
2024-01-01 01:28:49 +0300

    creating a main page called TasksPage.

---
+++ src/pages/TasksPage.jsx
@@ -0,0 +1,10 @@
+export default function TasksPage() {
+    return (
+        <>
+            <nav className="w-full p-5 border-4 border-b-black">
+                <div className="logo">Jadwal</div>
+            </nav>
+
+        </>
+    )
+}
