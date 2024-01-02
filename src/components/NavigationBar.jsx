68a72a696ee4c0fd5ad555c1f037675d9210d218
SaadAlsayed <smqaa.github@gmail.com>
2024-01-03 01:01:32 +0300

    finished the structure of navigation bar, i will style it later

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,9 +2,11 @@ export default function NavigationBar(){
     return (
         <>
             <nav className="w-full p-5 border-4 border-b-black">
-                <div className="logo">Jadwal</div>
-                <div className="">
-
+                <div className="grid grid-cols-2 gap-x-2 ">
+                    <div className="flex justify-start p-2"> <h1>Jadwal</h1></div>
+                    <div className="flex justify-end">
+                        <button className="border p-2">dfdsf</button>
+                    </div>
                 </div>
             </nav>
         </>
