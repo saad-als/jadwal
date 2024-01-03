b22b3365e6d317e590949c4f3e2776b1934044f2
SaadAlsayed <smqaa.github@gmail.com>
2024-01-03 17:01:15 +0300

    changing the main language of the website to arabic

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,10 +3,10 @@ export default function NavigationBar(){
         <>
             <nav className="w-full p-5 border-4 border-b-black">
                 <div className="grid grid-cols-2 gap-x-2 ">
-                    <div className="flex justify-start p-2"> <h1>Jadwal</h1></div>
-                    <div className="flex justify-end">
-                        <button className="border p-2">dfdsf</button>
+                    <div className="flex justify-start">
+                        <button className="border p-2">القائمة</button>
                     </div>
+                    <div className="flex justify-end p-2"> <h1>جدول</h1></div>
                 </div>
             </nav>
         </>
