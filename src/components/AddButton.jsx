5e5374aa5224d3a810cd8131e8155d2961c8b9a9
SaadAlsayed <smqaa.github@gmail.com>
2024-01-04 11:06:48 +0300

    a button to add new tasks

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,6 +1,9 @@
-export default function AddButton(){
+export default function AddButton() {
     return (
         <>
+            <div className="p-3">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg flex justify-center">إضافة</button>
+            </div>
         </>
     );
 }
