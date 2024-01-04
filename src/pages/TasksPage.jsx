2aed2bedfa786b78b21bb943d50ca4d443248c28
SaadAlsayed <smqaa.github@gmail.com>
2024-01-04 10:43:48 +0300

    added footer, and trying to structure the page

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -3,18 +3,29 @@ export default function TasksPage() {
     return (
         <>
 
-            <div className="grid grid-rows-4">
+            <div className="grid grid-rows-4 relative">
+                {/* first row */}
                 <div>
                     <NavigationBar />
                 </div>
-
+                {/* second row */}
                 <div className="grid grid-cols-2">
-                    <div>main</div>
-                    <div className="flex justify-items-end">side</div>
+                    <div className="flex justify-center p-5 ">main</div>
+                    <div className="flex justify-center p-5 ">side</div>
                 </div>
+
+                {/* third row */}
+
+
+
+                {/* fourth row */}
 
             </div>
 
+
+            <footer className="flex justify-center absolute inset-x-0 bottom-0 p-4 ">
+                <p className="text-slate-500">جميع الحقوق محفوظة @2024</p>
+            </footer>
         </>
     )
 }
