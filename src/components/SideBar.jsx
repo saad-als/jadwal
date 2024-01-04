03cadb644264a727e29e77fd0f226b9a63b6ec98
SaadAlsayed <smqaa.github@gmail.com>
2024-01-04 10:54:36 +0300

    basic structure for the side bar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+    return (
+
+        <>
+            <div className="grid grid-rows-2 gap-4">
+                <div className="flex justify-center p-2 border rounded-md"><a href="#">المهام</a></div>
+                <div className="flex justify-center p-2 border rounded-md"><a href="#">التقويم</a></div>
+
+            </div>
+
+        </>
+    );
+}
