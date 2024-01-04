b8df409944c90d8fcd32ae674863d155ab167cd9
SaadAlsayed <smqaa.github@gmail.com>
2024-01-05 00:33:42 +0300

    adding task card

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,6 +1,13 @@
 export default function TaskCard() {
     return (
         <>
+            <div className="container w-full h-12 my-6 border-solid border-2 border-sky-500 rounded-md">
+
+                <div>4/1/2024</div>
+                <div>العنوان والمهمة</div>
+                <input type="checkbox" />
+
+            </div>
 
         </>
     );
