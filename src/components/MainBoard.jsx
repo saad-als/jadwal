459af2d665f77198bd8d85c8a833a203466dfd6b
SaadAlsayed <smqaa.github@gmail.com>
2024-01-06 00:23:56 +0300

    Basic structure for the main board, i need to center it and add nice stryle to it

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,27 +1,10 @@
 import TaskCard from "./TaskCard";
 export default function MainBoard() {
+
     return (
         <>
-            <div className="container h-64  overflow-auto overscroll-contain">
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
+            <div className="w-80 h-80 p-5 border overflow-auto overscroll-contain ">
+
                 <TaskCard />
                 <TaskCard />
                 <TaskCard />
