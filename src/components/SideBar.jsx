b84d7b07d3e973cef610abc8d17aa84ab7253334
SaadAlsayed <smqaa.github@gmail.com>
2024-01-05 20:23:01 +0300

    changed from grid style to flex and cols, missing styling

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,19 @@
+import AddButton from "./AddButton"
 export default function SideBar() {
     return (
 
         <>
-            <div className="grid grid-rows-2 gap-4">
-                <div className="flex justify-center p-2 border rounded-md"><a href="#">المهام</a></div>
-                <div className="flex justify-center p-2 border rounded-md"><a href="#">التقويم</a></div>
+            <div className="absolute right-3 w-64 border flex flex-col justify-center">
+                <div className="flex flex-col space-y-4">
+                    <div className="flex justify-center p-2 border rounded-md"><a href="#">المهام</a></div>
+                    <div className="flex justify-center p-2 border rounded-md"><a href="#">التقويم</a></div>
 
+                </div>
+
+                <div className="flex justify-center ">
+                    <AddButton />
+                </div>
             </div>
-
         </>
     );
 }
