b5abfcb861a31c652c00ee8c845be8d7fb7951c0
SaadAlsayed <smqaa.github@gmail.com>
2024-01-06 00:24:48 +0300

    this is not the final design, missing functionality

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,11 +1,12 @@
 export default function TaskCard() {
     return (
         <>
-            <div className="container w-full h-12 my-6 border-solid border-2 border-sky-500 rounded-md">
+            <div className="container w-full h-12 my-6 flex justify-between border-solid border-2  border-sky-500 rounded-md">
 
-                <div>4/1/2024</div>
-                <div>العنوان والمهمة</div>
-                <input type="checkbox" />
+                <div className="m-2">2024</div>
+                <div className="m-2">العناون</div>
+                <div className="m-2"><input type="checkbox" /></div>
+
 
             </div>
 
