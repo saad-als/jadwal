c9636dbb4aed4fdbf8d4f28409c7a9e00948a496
SaadAlsayed <smqaa.github@gmail.com>
2024-01-05 20:24:08 +0300

    this structure is way better than grid

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,44 +1,30 @@
 import NavigationBar from "../components/NavigationBar"
 import MainBoard from "../components/MainBoard"
 import SideBar from "../components/SideBar"
-import AddButton from "../components/AddButton"
 export default function TasksPage() {
     return (
         <>
 
-            <div className="grid grid-rows-3 relative">
-                {/* first row */}
+            <div>
                 <div>
                     <NavigationBar />
                 </div>
-
-                {/* second row */}
-                <div className="grid grid-cols-4">
-                    <div className="flex justify-center p-5 w-full col-span-3">
-                        <MainBoard />
-                    </div>
-
-
-                    <div className="flex justify-center p-5 grid grid-rows-2">
-                        <SideBar />
-                        <div className="flex justify-end">
-                            <AddButton />
-                        </div>
-                    </div>
+                <div className="flex m-5 container ">
+                    <MainBoard />
+                    <SideBar />
 
                 </div>
 
-
-                {/* third row */}
-
                 <div>
+
                     <footer className="flex justify-center absolute inset-x-0 bottom-0 p-4 ">
                         <p className="text-slate-500">جميع الحقوق محفوظة @2024</p>
                     </footer>
 
                 </div>
-
             </div>
+
+
 
 
 
